3f17422e6e0889a2a71cfb55029b42346355394e
Shivani Sehgal <shivani.sehgal@daffodilsw.com>
2021-12-12 19:32:07 +0530

    Routing setup

--- portfolio/src/App/App.js
+++ portfolio/src/App/App.js
@@ -1,9 +1,39 @@
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+
+import { routeComponentMappings } from '../routing/routeComponentMappings';
+import { routes } from '../routing/routes';
 import './App.css';
 
 function App() {
+  const renderRoutes = () => (
+    <Routes>
+      {
+        Object.keys(routes).map((route) => {
+          const ComponentByRoute = routeComponentMappings[routes[route]];
+          return (
+            <Route path={routes[route]} element={<ComponentByRoute />} />
+          )
+        })
+      }
+    </Routes>
+  );
+
+  const renderLinks = () => (
+    Object.keys(routes).map((route) => (
+      <div>
+        <Link to={routes[route]}>{route}</Link>
+      </div>
+    ))
+  );
+
   return (
     <div className="App">
-      <h1>Portfolio</h1>
+      <h1>Portfolio Header</h1>
+      <Router>
+        {renderRoutes()}
+        {renderLinks()}
+      </Router>
+      <h1>Portfolio Footer</h1>
     </div>
   );
 }
